7beafc12d12cb7a484581f9a445b8b115723baf5
charlottefrance <82273675+charlottefrance@users.noreply.github.com>
2022-01-09 20:21:42 +0000

    Add files via upload

--- src/js7-classes/challenge.js
+++ src/js7-classes/challenge.js
@@ -27,7 +27,10 @@ export class Coordinate {
    * @param {number} xCoord - 22.
    * @param {number} yCoord - 20.
    */
-  constructor(xCoord, yCoord) {}
+  constructor(xCoord, yCoord) {
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  }
 }
 
 /**
@@ -48,14 +51,17 @@ export class Alert {
    * Create an alert.
    * @param {string} message - "Are sure you want to proceed?"
    */
-  constructor() {}
+  constructor() {
+    this.message = message;
+  }
 
   /**
    * Returns a string with "!!!! " + " !!!!" added to the start and end of the message key.
    * @return {string} "!!!! Are sure you want to proceed? !!!!"
    */
   printMessage() {
-    return;
+    const message = `!!!! ${this.message} !!!!`
+    return message;
   }
 }
 
